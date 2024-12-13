1a40d8d49af0619b03eeec96dd169f4502f6c635
ShySheridan <143121281+ShySheridan@users.noreply.github.com>
2024-12-13 19:50:22 +0300

    task 4

---
+++ opd/lab01/lab01-4.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+chmod u+r arbok4
+chmod u+r combusken6
+chmod u+r ledyba
+chmod u+r mandibuzz8
+
+echo '#1'
+#shopt -s globstar
+#wc -m $(ls -dp1 **/*a lab0 | grep -v "/$") | sort -nk1
+grep --include='*a' -r . | wc -m | sort -n
+
+#ls -d показывает директории без раскрытия их содержимого
+#ls -p добавляет / к имени, если это директория.
+#ls -1 выводит один элемент на строку.
+
+echo '#2'
+ls -lR lab0 2>/tmp/errors.log | grep "sha" | sort -k2,2nr
+
+echo '#3'
+#ls -1 -dp **/*t lab0 | grep -v "/$" | cat $(ls -1 -dp **/*t lab0 | grep -v "/$") | sort 
+grep -r --include="*t" $ lab0 | sort -t ':' -k1,1
+# $ - чтоб не было проверки на содержание строк
+
+echo '#4'
+ls -ltR --time-style=long-iso lab0/weezing2 2>&1 | grep '^-' | sort -k6,7
+
+echo '#5'
+ls -lR lab0/weezing2 2>/tmp/errors.log | grep '^-' | sort
+
+echo '#6'
+ls -lR lab0 | grep '\-.*t$' | sort -k5,5n
+
+chmod u-r arbok4
+chmod u-r combusken6
+chmod u-r ledyba
+chmod u-r mandibuzz8
